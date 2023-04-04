289e5fc9d3a1ae25eb248843b73c549afc77edb4
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-04 22:59:23 +0700

    1.3.0

--- Double3.cs
+++ Double3.cs
@@ -10,6 +10,7 @@ namespace Numerics
             Y = 1,
             Z = 1
         };
+
         public static readonly Double3 Zero = new()
         {
             X = 0,
@@ -20,6 +21,17 @@ namespace Numerics
         public double X;
         public double Y;
         public double Z;
+
+        public static Double3 Clamp(Double3 a, Double3 b, Double3 d)
+        {
+            return (b - a).SqrMagnitude() > d.SqrMagnitude() ? a + d : b;
+        }
+
+        public static Double3 Clamp(Vector3 a, Vector3 b, Double3 d)
+        {
+            var delta = b - a;
+            return delta.LengthSquared() > d.SqrMagnitude() ? a + d : b;
+        }
 
         public static implicit operator Double3(Vector3 value)
         {
@@ -46,6 +58,16 @@ namespace Numerics
             return a * (1 - t) + b * t;
         }
 
+        public static Double3 operator +(Vector3 a, Double3 b)
+        {
+            return new Double3
+            {
+                X = a.X + b.X,
+                Y = a.Y + b.Y,
+                Z = a.Z + b.Z
+            };
+        }
+
         public static Double3 operator +(Double3 a, Double3 b)
         {
             return new Double3
@@ -53,6 +75,16 @@ namespace Numerics
                 X = a.X + b.X,
                 Y = a.Y + b.Y,
                 Z = a.Z + b.Z
+            };
+        }
+
+        public static Double3 operator -(Double3 a, Double3 b)
+        {
+            return new Double3
+            {
+                X = a.X - b.X,
+                Y = a.Y - b.Y,
+                Z = a.Z - b.Z
             };
         }
 
@@ -84,6 +116,11 @@ namespace Numerics
                 Y = a * b.Y,
                 Z = a * b.Z
             };
+        }
+
+        public static implicit operator Vector3(Double3 value)
+        {
+            return new Vector3((float)value.X, (float)value.Y, (float)value.Z);
         }
     }
 }

e6687c9bffbfd178a5e4e23057e95247f87b96fb
hoang <hoang.tu@incepit.com>
2023-04-05 16:44:31 +0700

    1.5.0

--- Int3.cs
+++ Int3.cs
@@ -4,6 +4,48 @@ namespace Numerics
 {
     public struct Int3
     {
+        public static readonly Int3 Back = new()
+        {
+            X = 0,
+            Y = 0,
+            Z = -1
+        };
+
+        public static readonly Int3 Down = new()
+        {
+            X = 0,
+            Y = -1,
+            Z = 0
+        };
+
+        public static readonly Int3 Forward = new()
+        {
+            X = 0,
+            Y = 0,
+            Z = 1
+        };
+
+        public static readonly Int3 Left = new()
+        {
+            X = -1,
+            Y = 0,
+            Z = 0
+        };
+
+        public static readonly Int3 Right = new()
+        {
+            X = 1,
+            Y = 0,
+            Z = 0
+        };
+
+        public static readonly Int3 Up = new()
+        {
+            X = 0,
+            Y = 1,
+            Z = 0
+        };
+
         public int X;
         public int Y;
         public int Z;
@@ -53,7 +95,7 @@ namespace Numerics
             };
         }
 
-        public static bool operator==(Int3 a, Int3 b)
+        public static bool operator ==(Int3 a, Int3 b)
         {
             return a.X == b.X && a.Y == b.Y && a.Z == b.Z;
         }

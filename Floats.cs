f347383cd39fe339205792d8465618b5d5e0b94b
hoangtuincepit <128338344+hoangtuincepit@users.noreply.github.com>
2023-04-20 12:25:24 +0700

    1.10.0

--- Floats.cs
+++ Floats.cs
@@ -1,19 +1,42 @@
-﻿namespace Numerics
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Numerics
 {
-    public class Floats
+    public struct Floats
     {
-        private float _value;
+        private byte[] _iv;
+        private byte[] _key;
+        private byte[] _value;
 
         public static implicit operator float(Floats value)
         {
-            return value._value;
+            using var aes = Aes.Create();
+            aes.IV = value._iv;
+            aes.Key = value._key;
+
+            using var ms = new MemoryStream(value._value);
+            using var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read);
+            var bytes = new byte[sizeof(float)];
+            var _ = cs.Read(bytes, 0, bytes.Length);
+            return BitConverter.ToSingle(bytes, 0);
         }
 
         public static implicit operator Floats(float value)
         {
+            using var aes = Aes.Create();
+            using var ms = new MemoryStream();
+            using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                var bytes = BitConverter.GetBytes(value);
+                cs.Write(bytes, 0, bytes.Length);
+            }
             return new Floats
             {
-                _value = value
+                _iv = aes.IV,
+                _key = aes.Key,
+                _value = ms.ToArray()
             };
         }
     }
--- Ints.cs
+++ Ints.cs
@@ -2,18 +2,25 @@
 {
     public struct Ints
     {
+        private int _key;
         private int _value;
 
         public static implicit operator int(Ints value)
         {
-            return value._value;
+            return value._value ^ value._key;
         }
 
         public static implicit operator Ints(int value)
         {
+#if INTS_USE_DATE_TIME
+            var key = DateTime.Now.Millisecond;
+#else
+            const int key = 20042023;
+#endif
             return new Ints
             {
-                _value = value
+                _key = key,
+                _value = value ^ key
             };
         }
     }
